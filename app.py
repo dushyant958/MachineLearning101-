5e489c171282955a07b7fafb0087fc7ac720098e
dushyant958 <dushyantatalkar415@gmail.com>
2025-01-08 01:55:19 +0530

    App and Pipelines updates

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
 import sys
 import pandas as pd
 import os
 import dill
+
 
 def error_message_detail(error, error_detail:sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -46,8 +52,8 @@ class PredictPipeline:
         pass
     def predict(self, features):
         try:
-            model_path = 'artifact\model.pkl'
-            preprocessor_path = 'artifact\preprocessor.pkl'
+            model_path = 'D:\\ml_project\\artifact\\model.pkl'
+            preprocessor_path = 'D:\\ml_project\\artifact\\preprocessor.pkl'
             model = load_object(file_path = model_path)
             preprocessor = load_object(file_path = preprocessor_path)
             data_scaled = preprocessor.transform(features)
@@ -89,15 +95,54 @@ class CustomData:
         try:
             custom_data_input_dict = {
                 "gender": [self.gender],
-                "race_ethnicity": [self.race_ethnicity],
-                "parental_level_of_education": [self.parental_level_of_education],
+                "race/ethnicity": [self.race_ethnicity],
+                "parental level of education": [self.parental_level_of_education],
                 "lunch": [self.lunch],
-                "test_preparation_course": [self.test_preparation_course],
-                "reading_score": [self.reading_score],
-                "writing_score": [self.writing_score],
+                "test preparation course": [self.test_preparation_course],
+                "reading score": [self.reading_score],
+                "writing score": [self.writing_score],
             }
 
             return pd.DataFrame(custom_data_input_dict)
 
         except Exception as e:
             raise CustomException(e, sys)
+
+application = Flask(__name__)
+
+app = application
+
+##Route for our homepage
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('race/thnicity'),
+            parental_level_of_education=request.form.get('parental level of education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test preparation course'),
+            reading_score=float(request.form.get('writing score')),
+            writing_score=float(request.form.get('reading score'))
+
+        )
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
+
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
+    
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", debug=True)      
